fd5240c6ac5a9a2750340d696805cdc7b86e19ac
Chino Rodriguez <chino@Chinos-MacBook-Pro.local>
2022-04-30 19:55:47 -0700

    add start script

--- app.js
+++ app.js
@@ -139,6 +139,7 @@ app.use((err, req, res, next) => {
     res.status(err.statusCode).render('error', { err });
 })
 
-app.listen(3000, (req, res) => {
-    console.log('Listneing on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, (req, res) => {
+    console.log(`Listneing on port ${port}`);
 })
